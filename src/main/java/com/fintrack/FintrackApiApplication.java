143489c1961183cd44b973c6c7c8e2bdd56768fd
Igor Conrado <conradoigor78@gmail.com>
2026-03-08 18:29:33 -0300

    chore: initial project structure

---
+++ src/main/java/com/fintrack/FintrackApiApplication.java
@@ -0,0 +1,12 @@
+package com.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackApiApplication.class, args);
+    }
+}
